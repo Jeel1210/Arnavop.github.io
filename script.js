66b9550e274f6574bf0b8fb4f9cb36eafd7d22f4
Arnavop <arnavkamramalout@gmail.com>
2021-11-25 18:11:01 +0530

    update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Athelete", "Freelancer"],
+        strings: ["Student", "Developer", "Athlete", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer", "Athelete", "Freelancer"],
+        strings: ["Student", "Developer", "Athlete", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
